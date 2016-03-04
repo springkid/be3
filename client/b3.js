41a394c59e8a8d6ab4c1b4f45ad7febdc6538b25
jmgomezr Nitrous <jmgomezr@gmail.com>
2016-03-04 13:07:53 +0000

    Welcome page created, tabla de proyectos reestilo iniciado

--- client/b3.js
+++ client/b3.js
@@ -8,6 +8,16 @@ Router.configure({
 });
 
 Router.route('/', function () {
+  this.render('', {to: 'navbar'});
+  this.render('welcome', {to: 'content'});
+});
+
+Router.route('/projects', function () {
   this.render('navbar', {to: 'navbar'});
-  this.render('projects', {to: 'content'});  
+  this.render('projects', {to: 'content'});
+});
+
+Router.route('/clients', function () {
+  this.render('navbar', {to: 'navbar'});
+  this.render('clients', {to: 'content'});
 });
